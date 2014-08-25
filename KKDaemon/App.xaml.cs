39475b3cbdc34a5fe5349573dab5c6657efd0d30
kelly <chepy.v@gmail.com>
2014-08-25 22:42:21 +0800

    kkdaemon

---
+++ KKDaemon/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Diagnostics;
+using System.Threading;
+
+namespace KKDaemon
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get Reference to the current Process
+            Process thisProc = Process.GetCurrentProcess();
+            // Check how many total processes have the same name as the current one
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                // If ther is more than one, than it is already running.
+                MessageBox.Show("Application is already running.");
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ KKDaemon/MainWindow.xaml.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.IO;
+using SimpleJson;
+
+namespace KKDaemon
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string PyExecFile
+        {
+            get
+            {
+                return Config["PyExe"] as string;
+            }
+        }
+        private const string ConfigFilePath = "config.json";
+        private NotifyIcon notifyIcon;
+        short hotkey_flag = 1;
+
+        JsonObject Config;
+
+        Dictionary<System.Windows.Controls.MenuItem, string> MenuItem2MenuScriptMap = new Dictionary<MenuItem, string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ChDirToThisExe();
+            InitConfigs();
+
+            InitNotifyIcon();
+
+            InitMenus();
+            InitTimer();
+            this.Show();
+            this.Hide();
+        }
+
+        private void InitTimer()
+        {
+            int tipsInterval = 5;  // 默認5秒
+            if (Config.ContainsKey("TipsInterval"))
+            {
+                var o = Config["TipsInterval"]; // 間隔時間
+                tipsInterval = int.Parse(o.ToString());
+            }
+
+            System.Windows.Threading.DispatcherTimer dTimer = new System.Windows.Threading.DispatcherTimer();
+            dTimer.Tick += new EventHandler((object sender, EventArgs e) => 
+            {
+                this.notifyIcon.BalloonTipText = GetRandTips();
+                this.notifyIcon.ShowBalloonTip(500);
+            });
+            dTimer.Interval = new TimeSpan(0, 0, tipsInterval);
+            dTimer.Start(); 
+        }
+
+        
+        private void InitConfigs()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                MessageBox.Show("找不到配置文件 " + ConfigFilePath);
+            }
+            string jsonTxt = File.ReadAllText(ConfigFilePath);
+
+            JsonObject jsonObj = SimpleJson.SimpleJson.DeserializeObject(jsonTxt) as JsonObject;
+
+            Config = jsonObj;
+        }
+
+        /// <summary>
+        /// 註冊全局快捷鍵
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // 快捷键注册
+            WindowInteropHelper wih = new WindowInteropHelper(this);
+            HotKey.RegisterHotKey(wih.Handle, hotkey_flag, HotKey.KeyModifiers.Alt, (int)System.Windows.Forms.Keys.Oemtilde);  // alt + ~
+            HwndSource hs = HwndSource.FromHwnd(wih.Handle);
+            hs.AddHook(new HwndSourceHook(OnHotKey));
+        }
+
+        /// <summary>
+        /// 系统快捷键响应
+        /// </summary>
+        /// <param name="hwnd"></param>
+        /// <param name="msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <param name="handled"></param>
+        /// <returns></returns>
+        private IntPtr OnHotKey(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case HotKey.WM_HOTKEY:
+                    {
+                        int sid = wParam.ToInt32();
+                        if (sid == hotkey_flag)
+                        {
+                            //MessageBox.Show("按下Alt+S");
+                            ShowMyMenu();
+                        }
+                        handled = true;
+                        break;
+                    }
+            }
+            return IntPtr.Zero;
+        }
+
+        // 工作目錄切換到這個exe文件夾內
+        void ChDirToThisExe()
+        {
+            string myExePath = System.Reflection.Assembly.GetEntryAssembly().Location; // 切換本exe的目錄
+            string myExeDir = System.IO.Path.GetDirectoryName(myExePath);
+            System.IO.Directory.SetCurrentDirectory(myExeDir);
+        }
+
+        void InitMenus()
+        {
+            this._Button.ContextMenu = new System.Windows.Controls.ContextMenu();
+
+            System.Windows.Controls.MenuItem title = new System.Windows.Controls.MenuItem();
+            title.Header = @"KKDaemon";
+            title.Click += (__, __2) =>
+            {
+                ShowAbout(null, null);
+            };
+            this._Button.ContextMenu.Items.Add(title);
+            this._Button.ContextMenu.Items.Add(new System.Windows.Controls.Separator());
+
+
+            foreach (KeyValuePair<string, object> kv in (JsonObject)Config["Menus"])
+            {
+                System.Windows.Controls.MenuItem menuItem = new System.Windows.Controls.MenuItem();
+                menuItem.Header = kv.Key;
+                MenuItem2MenuScriptMap[menuItem] = kv.Value as string;
+
+                menuItem.Click += OnClickMenu;
+                this._Button.ContextMenu.Items.Add(menuItem);
+            }
+
+            System.Windows.Controls.MenuItem exit2 = new System.Windows.Controls.MenuItem();
+            this._Button.ContextMenu.Items.Add(new System.Windows.Controls.Separator());
+            this._Button.ContextMenu.Items.Add(exit2);
+            exit2.Header = @"殘忍地退出";
+            exit2.Click += ExitApp;
+
+            this._Button.ContextMenu.IsOpen = false;
+        }
+
+        void OnClickMenu(object sender, RoutedEventArgs e)
+        {
+            string scriptFile = this.MenuItem2MenuScriptMap[(MenuItem)sender];
+            ChDirToThisExe();
+
+            string fullPath = System.IO.Path.GetFullPath(scriptFile);  // 完整路徑
+            string dirPath = System.IO.Path.GetDirectoryName(fullPath);
+            string pyExeFullPath = System.IO.Path.GetFullPath(PyExecFile);
+            System.IO.Directory.SetCurrentDirectory(dirPath); // chdir
+            string pyExeName = System.IO.Path.GetFileName(PyExecFile);
+
+            if (Directory.Exists(fullPath))
+            {
+                System.Diagnostics.Process.Start("explorer.exe", fullPath);
+            }
+            else
+            {
+                switch (System.IO.Path.GetExtension(scriptFile))
+                {
+                    case ".py":
+                        System.Diagnostics.Process.Start(pyExeFullPath, fullPath);
+                        break;
+                    case ".bat":
+                        System.Diagnostics.Process.Start(fullPath);
+                        break;
+                    default:
+                        throw new Exception("UnSupport file " + fullPath);
+                }
+            }
+
+        }
+        /// <summary>
+        /// 菜单~
+        /// </summary>
+        void ShowMyMenu()
+        {
+            
+            this.Activate();  // 激活窗口~ 失焦時關閉菜單
+
+            this._Button.ContextMenu.IsOpen = true;
+
+        }
+
+        private void ShowAbout(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = System.Windows.WindowState.Normal;
+        }
+
+
+        private void HideAbout(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void ExitApp(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            e.Cancel = true;
+            HideAbout(null, null);
+
+        }
+
+        string GetRandTips()
+        {
+            var rand = new System.Random();
+
+            JsonArray tipsArray = Config["Tips"] as JsonArray;
+            int randNum = rand.Next(0, tipsArray.Count);
+
+            return tipsArray[randNum] as string;
+        }
+        private void InitNotifyIcon()
+        {
+            this.notifyIcon = new NotifyIcon();
+
+            this.notifyIcon.BalloonTipText = GetRandTips();
+            this.notifyIcon.Text = GetRandTips();
+
+            this.notifyIcon.Icon = Properties.Resources.icon;
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.ShowBalloonTip(500);
+
+            System.Windows.Application.Current.Exit += (object sender, ExitEventArgs exitEvent) =>
+            {
+                this.notifyIcon.Dispose();
+            };
+
+            this.notifyIcon.MouseClick += notifyIcon_MouseClick;
+            this.notifyIcon.MouseDoubleClick += (o, _e) =>
+            {
+                if (_e.Button == System.Windows.Forms.MouseButtons.Left) this.ShowAbout(o, _e);
+            };
+        }
+
+        void notifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                ShowMyMenu();
+            else
+                ShowAbout(sender, e);
+        }
+
+        private void _Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //ExitApp(null, null);
+            HideAbout(null, null);
+        }
+
+    }
+}
+
+class HotKey
+{
+    /// <summary> 
+    /// 如果函数执行成功，返回值不为0。 
+    /// 如果函数执行失败，返回值为0。要得到扩展错误信息，调用GetLastError。.NET方法:Marshal.GetLastWin32Error() 
+    /// </summary> 
+    /// <param name="hWnd">要定义热键的窗口的句柄</param> 
+    /// <param name="id">定义热键ID（不能与其它ID重复） </param> 
+    /// <param name="fsModifiers">标识热键是否在按Alt、Ctrl、Shift、Windows等键时才会生效</param> 
+    /// <param name="vk">定义热键的内容,WinForm中可以使用Keys枚举转换， 
+    /// WPF中Key枚举是不正确的,应该使用System.Windows.Forms.Keys枚举，或者自定义正确的枚举或int常量</param> 
+    /// <returns></returns> 
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool RegisterHotKey(
+    IntPtr hWnd,
+    int id,
+    KeyModifiers fsModifiers,
+    int vk
+    );
+    /// <summary> 
+    /// 取消注册热键 
+    /// </summary> 
+    /// <param name="hWnd">要取消热键的窗口的句柄</param> 
+    /// <param name="id">要取消热键的ID</param> 
+    /// <returns></returns> 
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool UnregisterHotKey(
+    IntPtr hWnd,
+    int id
+    );
+    /// <summary> 
+    /// 向全局原子表添加一个字符串，并返回这个字符串的唯一标识符,成功则返回值为新创建的原子ID,失败返回0 
+    /// </summary> 
+    /// <param name="lpString"></param> 
+    /// <returns></returns> 
+    [DllImport("kernel32", SetLastError = true)]
+    public static extern short GlobalAddAtom(string lpString);
+    [DllImport("kernel32", SetLastError = true)]
+    public static extern short GlobalDeleteAtom(short nAtom);
+    /// <summary> 
+    /// 定义了辅助键的名称（将数字转变为字符以便于记忆，也可去除此枚举而直接使用数值） 
+    /// </summary> 
+    [Flags()]
+    public enum KeyModifiers
+    {
+        None = 0,
+        Alt = 1,
+        Ctrl = 2,
+        Shift = 4,
+        WindowsKey = 8
+    }
+    /// <summary> 
+    /// 热键的对应的消息ID 
+    /// </summary> 
+    public const int WM_HOTKEY = 0x312;
+}
